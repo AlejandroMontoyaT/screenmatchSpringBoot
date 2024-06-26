88f0a09650a84a3e1fba17650f8d68525ec7f5a5
Alejandro Montoya <isc.alejandro14@gmail.com>
2024-06-25 22:20:50 -0600

    Se realiza consumo de API

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.service.ConsumoAPI;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,11 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Este hola mundo viene desde el CommandLineRunner con Spring Boot!");
+		var consumoAPI = new ConsumoAPI();
+		//var json = consumoAPI.obtenerDatos("https://omdbapi.com/?t=game+of+thrones&apikey=4fc7c187");
+		var json = consumoAPI.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+	//	System.out.println("Este hola mundo viene desde el CommandLineRunner con Spring Boot!");
 	}
 }
