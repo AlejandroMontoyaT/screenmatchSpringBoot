eb4165ebec6c8b016b125822f81a1481b4fb694f
Alejandro Montoya <isc.alejandro14@gmail.com>
2024-06-27 11:33:30 -0600

    Se realiza el consumo de la APPI

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,7 @@
 package com.aluracursos.screenmatch;
 
 
+import com.aluracursos.screenmatch.model.DatosEpisodio;
 import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
@@ -27,5 +28,10 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		ConvierteDatos conversor = new ConvierteDatos();
 		var datos = conversor.obtenerDatos(json, DatosSerie.class);
 		System.out.println(datos);
+
+        //convertir los datos del episodio a un objeto DatosEpisodio
+        json= consumoAPI.obtenerDatos("https://omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=4fc7c187");
+        DatosEpisodio episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
+        System.out.println(episodios);
 	}
 }
