7b5fccffde9154a8c48626a74788a9a8a1469233
Alejandro Montoya <isc.alejandro14@gmail.com>
2024-06-26 19:12:47 -0600

    se realiza interface para que se lea el json

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,9 @@
 package com.aluracursos.screenmatch;
 
+
+import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
+import com.aluracursos.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,9 +19,13 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoAPI = new ConsumoAPI();
-		//var json = consumoAPI.obtenerDatos("https://omdbapi.com/?t=game+of+thrones&apikey=4fc7c187");
-		var json = consumoAPI.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
+		var json = consumoAPI.obtenerDatos("https://omdbapi.com/?t=game+of+thrones&apikey=4fc7c187");
+		//	System.out.println("Este hola mundo viene desde el CommandLineRunner con Spring Boot!");
+	//	var json = consumoAPI.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
 		System.out.println(json);
-	//	System.out.println("Este hola mundo viene desde el CommandLineRunner con Spring Boot!");
+
+		ConvierteDatos conversor = new ConvierteDatos();
+		var datos = conversor.obtenerDatos(json, DatosSerie.class);
+		System.out.println(datos);
 	}
 }
