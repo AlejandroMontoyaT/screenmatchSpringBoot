8ad9d64c9318b3d77849e6834f75730138ff28d8
Alejandro Montoya <isc.alejandro14@gmail.com>
2024-07-08 17:23:04 -0600

    Se realiza refactorizacion pero tiene un bug de la conulta no detectalos espacios al momento de realizar la consulta de la API

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -4,12 +4,14 @@ package com.aluracursos.screenmatch;
 import com.aluracursos.screenmatch.model.DatosEpisodio;
 import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.model.DatosTemporadas;
+import com.aluracursos.screenmatch.principal.Principal;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +25,13 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		//SE CREA NUEVA INSTANCIA DESPUESDE QUE SE ELIMINA LA PARTE DE ABAJO QUE ES MUY REPETITIVO VOLVER A PEDIR EL VALOR
+		Principal principal = new Principal();
+		principal.muestraElMenu();
+
+
+		/**
+		 ESTA PARTE SE ELIMINA POR QUE  SERIA REPETITIVO VOLVER A CONSULTAR EL MISMO DATO VARIAS VECES
 		var consumoAPI = new ConsumoAPI();
 		var json = consumoAPI.obtenerDatos("https://omdbapi.com/?t=game+of+thrones&apikey=4fc7c187");
 		//	System.out.println("Este hola mundo viene desde el CommandLineRunner con Spring Boot!");
@@ -37,16 +46,17 @@ public class ScreenmatchApplication implements CommandLineRunner {
         json= consumoAPI.obtenerDatos("https://omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=4fc7c187");
         DatosEpisodio episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
         System.out.println(episodios);
+		 */
 
-		//crear lista
-		List<DatosTemporadas> temporadas = new ArrayList<>();
+		//crear lista para refacotrizar esto se pasa a la clase principal
+		//List<DatosTemporadas> temporadas = new ArrayList<>();
 		//se crea un for para la busqueda de a listas
-		for (int i = 1; i <= datos.totalDeTemporadas() ; i++) {
-			json= consumoAPI.obtenerDatos("https://omdbapi.com/?t=game+of+thrones&Season="+i+"&apikey=4fc7c187");
+		//for (int i = 1; i <= datos.totalDeTemporadas() ; i++) {
+		//	json= consumoAPI.obtenerDatos("https://omdbapi.com/?t=game+of+thrones&Season="+i+"&apikey=4fc7c187");
 			//convertir los datos
-			var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
-			temporadas.add(datosTemporadas);
-		}
-		temporadas.forEach(System.out::println);
+		//	var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
+		//	temporadas.add(datosTemporadas);
+		//}
+		//temporadas.forEach(System.out::println);
 	}
 }
---
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -0,0 +1,56 @@
+package com.aluracursos.screenmatch.principal;
+
+import com.aluracursos.screenmatch.model.DatosEpisodio;
+import com.aluracursos.screenmatch.model.DatosSerie;
+import com.aluracursos.screenmatch.model.DatosTemporadas;
+import com.aluracursos.screenmatch.service.ConsumoAPI;
+import com.aluracursos.screenmatch.service.ConvierteDatos;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static org.springframework.boot.web.servlet.server.Session.SessionTrackingMode.URL;
+
+public class Principal {
+
+    //para leer el valor de la instancia desde el teclado
+    private Scanner teclado = new Scanner(System.in);
+    //se crea instancia de clase consumo api
+    private ConsumoAPI consumoApi = new ConsumoAPI();
+
+    private final String URL_BASE = "https://omdbapi.com/?t=";
+
+    private final String API_KEY = "&apikey=4fc7c187";
+
+    // se crea un conversor de datos para traer el conviertedatos del screenmatch
+    private ConvierteDatos conversor = new ConvierteDatos();
+
+    public void muestraElMenu(){
+      //  System.out.println("Bienvenido a Screenmatch");
+        System.out.println("Escribe el nombre de la serie que deseas buscar:");
+      //busca los datos generaloes de la serie
+        var nombreSerie = teclado.nextLine();
+        //para obtener los datos de la serie desde la url
+        var json = consumoApi.obtenerDatos (URL + nombreSerie.replace(" " , "+") + API_KEY);
+        //se crea la variable para converit los datos
+        var datos = conversor.obtenerDatos(json, DatosSerie.class);
+        System.out.println(datos);
+
+        //Busca los datos de todas las temporadas
+        List<DatosTemporadas> temporadas = new ArrayList<>();
+        //se crea un for para la busqueda de a listas
+        for (int i = 1; i <= datos.totalDeTemporadas() ; i++) {
+            json = consumoApi.obtenerDatos(URL + nombreSerie.replace(" ", "+") + "&Season=" + i + API_KEY);
+            //convertir los datos
+            var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
+            temporadas.add(datosTemporadas);
+        }
+        temporadas.forEach(System.out::println);
+
+        //se imprime el valor de json
+       // System.out.println(json);
+
+    }
+}
+
